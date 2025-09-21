2f639ea2663039d24454bfb7423e9f918a6039b9
yashgupta7337 <yashgupta7337@gmail.com>
2025-09-21 17:04:52 +0530

    Initial Commit

---
+++ 01_cpp_basics/01_cpp_basics.cpp
@@ -0,0 +1,12 @@
+/*
+INTRODUCTION TO PROGRAMMING
+# assembly -> (compiler) -> computer code -> (interpreter) -> output
+
+*/
+# include <iostream> // input output stream -> c++ library to handle input and output 
+using namespace std;
+
+int main() { // main function - starting point of the program if not specified otherwise
+    cout << "Hello, World!" << endl; // cout - console output, << - insertion operator, endl - end line
+    return 0;
+}
